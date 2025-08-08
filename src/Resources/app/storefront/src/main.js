c811d106d99be67b996dbd193e92022817c1a219
Melos <statovcimelos96@gmail.com>
2025-08-08 15:58:03 +0200

    Made Some Ui Changes

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,4 +1,7 @@
 import AjaxPlugin from './ajax';
+import EasterConfettiPlugin from './plugin/easter-confetti/easter-confetti.plugin';
 
 const PluginManager = window.PluginManager;
 PluginManager.register('AjaxPlugin',AjaxPlugin,'[data-ajax-plugin]');
+PluginManager.register('EasterConfetti', EasterConfettiPlugin, '[data-easter-confetti]');
+
